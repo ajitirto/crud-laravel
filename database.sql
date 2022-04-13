ccd31578b915e39e72966825eeba277948666968
ajitirto <ajiwoke@gmail.com>
2022-04-13 13:41:42 +0700

    menambah sql

---
+++ database.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 13, 2022 at 08:40 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud_aji`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `articles`
+--
+
+CREATE TABLE `articles` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `judul` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `articles`
+--
+
+INSERT INTO `articles` (`id`, `judul`, `created_at`, `updated_at`) VALUES
+(1, 'Placeat saepe ea possimus provident quos est molestiae reiciendis.', NULL, NULL),
+(2, 'Totam laudantium molestiae similique sit.', NULL, NULL),
+(3, 'Aut consequatur ducimus ut non voluptatem voluptas.', NULL, NULL),
+(4, 'Ad sit voluptatem qui ut dolorem.', NULL, NULL),
+(5, 'Qui consequatur eum fuga corrupti.', NULL, NULL),
+(6, 'Quos nesciunt blanditiis amet odio.', NULL, NULL),
+(7, 'Ex doloremque consequuntur velit alias repellendus ullam.', NULL, NULL),
+(8, 'Perspiciatis a quo beatae nobis et suscipit illo.', NULL, NULL),
+(9, 'Maiores voluptate animi est enim totam.', NULL, NULL),
+(10, 'Rerum expedita inventore nulla voluptates perferendis placeat.', NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
